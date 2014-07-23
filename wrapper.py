c7c754f3cf5c12a0e733f0267bb5d803505fa7aa
OZv <voc_bt@yahoo.co.jp>
2014-07-23 14:01:09 +0800

    Update wrapper.py

--- wrapper.py
+++ wrapper.py
@@ -308,7 +308,7 @@ def gen_wordlist(ordered):
     sty.extend(['</style><script>function v(c,n){with(c.parentNode){var b=nextSibling;var i=parseInt(b.value);if(i==n)return;b.value=n;with(childNodes[i].style){color="";border="1px solid gray";backgroundColor="";}b=b.nextSibling.nextSibling;u(b.childNodes[n],b.nextSibling);}with(c.style){color="#369";border="1px solid #369";backgroundColor="#CEE3F6";}}function d(w){var n=parseInt(w/90)+1;return n*90-w;}',
     'function u(p,l){l.innerHTML=p.innerHTML;var n=document.createElement("span");n.style.visibility="hidden";l.appendChild(n);var h="";var w=0;for(var i=0;i<l.childNodes.length-1;i++){with(l.childNodes[i]){if(typeof(offsetWidth)=="undefined"){n.innerText=nodeValue;w=n.offsetWidth;h+="<span style=\\"display:inline-block;white-space:nowrap;margin-right:"+d(w)+"px\\">"+nodeValue+" </span>";}else if(offsetWidth){innerText+=" ";w=offsetWidth;with(style){marginRight=d(w)+"px";whiteSpace="nowrap";}h+="<span>"+outerHTML+" </span>";}}}n.innerText="";l.innerHTML=h;}',
     'function w(){var v=document.getElementsByTagName("div");for(var i=0;i<v.length;i++){with(v[i]){if(id=="Z1w"){var n=parseInt(previousSibling.previousSibling.previousSibling.value);u(previousSibling.childNodes[n],v[i]);}}}}',
-    'document.onreadystatechange=function(){if(document.readyState=="interactive"){w();if(!window.ActiveXObject)window.onresize=w;}};</script>'])
+    'F=0;function i(){if(!F){F=1;w();if(!window.ActiveXObject&&window.addEventListener)window.addEventListener("resize",w,false);}}if(window.addEventListener)window.addEventListener("load",i,false);else window.attachEvent("onload",i);</script>'])
     levels = [2000, 1500, 1500, 1500, 1500, 2000, 2000, 3000, 2000, 3000]
     ldict = {}
     i = 1
