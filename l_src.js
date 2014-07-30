c42eca06eda6d678d2238cfbf4b1f0102d4a3229
OZv <voc_bt@yahoo.co.jp>
2014-07-30 09:54:12 +0800

    Update l_src.js

--- l_src.js
+++ l_src.js
@@ -14,21 +14,24 @@
 // GNU General Public License for more details.
 function v(c,n){
 with(c.parentNode){
-var b=nextSibling;
-var i=parseInt(b.value);if(i==n)return;
-b.value=n;
-with(childNodes[i].style){
-color="";
-border="1px solid gray";
-backgroundColor="";
-}
-b=b.nextSibling.nextSibling;
-u(b.childNodes[n],b.nextSibling);
+	var b=nextSibling;
+	var i=parseInt(b.value);
+	if(i==n)return;
+	b.value=n;
+	with(childNodes[i].style){
+		color="";
+		border="1px solid gray";
+		boxShadow="";
+		backgroundColor="";
+	}
+	b=b.nextSibling.nextSibling;
+	u(b.childNodes[n],b.nextSibling);
 }
 with(c.style){
-color="#369";
-border="1px solid #369";
-backgroundColor="#CEE3F6";
+	color="#369";
+	border="1px solid #369";
+	boxShadow="-1px -1px 3px #A9BCF5 inset";
+	backgroundColor="#CEE3F6";
 }
 }
 function d(w){
@@ -42,23 +45,21 @@ n.style.visibility="hidden";
 l.appendChild(n);
 var h="";
 var w=0;
-for(var i=0;i<l.childNodes.length-1;i++){
+for(var i=0;i<l.childNodes.length-1;i++)
 with(l.childNodes[i]){
-if(typeof(offsetWidth)=="undefined"){
-n.innerText=nodeValue;
-w=n.offsetWidth;
-h+="<span style=\"margin-right:"+d(w)+"px\"class=w>"+nodeValue+" </span>";
-}
-else if(offsetWidth){
-innerText+=" ";
-w=offsetWidth;
-with(style){
-marginRight=d(w)+"px";
-whiteSpace="nowrap";
-}
-h+="<span>"+outerHTML+" </span>";
-}
-}
+	if(typeof(offsetWidth)=="undefined"){
+	n.innerText=nodeValue;
+	w=n.offsetWidth;
+	h+="<span style=\"margin-right:"+d(w)+"px\"class=w>"+nodeValue+" </span>";
+	}else if(offsetWidth){
+	innerText+=" ";
+	w=offsetWidth;
+	with(style){
+		marginRight=d(w)+"px";
+		whiteSpace="nowrap";
+	}
+	h+="<span>"+outerHTML+" </span>";
+	}
 }
 n.innerText="";
 l.innerHTML=h;
@@ -67,10 +68,10 @@ function w(){
 var v=document.getElementsByTagName("div");
 for(var i=0;i<v.length;i++)
 with(v[i]){
-if(id=="Z1w"){
-var n=parseInt(previousSibling.previousSibling.previousSibling.value);
-u(previousSibling.childNodes[n],v[i]);
-}
+	if(id=="Z1w"){
+	var n=parseInt(previousSibling.previousSibling.previousSibling.value);
+	u(previousSibling.childNodes[n],v[i]);
+	}
 }
 }
 F=0;
@@ -82,6 +83,5 @@ if(!window.ActiveXObject&&window.addEventListener)
 window.addEventListener("resize",w,false);
 }
 }
-if(window.addEventListener)
-window.addEventListener("load",i,false);
+if(window.addEventListener)window.addEventListener("load",i,false);
 else window.attachEvent("onload",i);
